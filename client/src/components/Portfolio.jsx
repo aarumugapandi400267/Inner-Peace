f257f946a351cbf074e7f166e1bccaddc56e81a1
Aarumugapandi <aarumugapandi400267@gmail.com>
2025-07-13 15:32:17 +0530

    small update

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -37,7 +37,7 @@ export default function Portfolio() {
   const [contact, setContact] = useState({ address: "", phone: "", email: "" });
   const [themeMode, setThemeMode] = useState("dark");
 
-  const domain="https://aarumugapandi400267.github.io"
+  const domain="https://aarumugapandi400267.github.io/Inner-Peace"
   useEffect(() => {
     fetch(domain+"/data/projects.json").then(res => res.json()).then(setProjects);
     fetch(domain+"/data/skills.json").then(res => res.json()).then(setSkills);
